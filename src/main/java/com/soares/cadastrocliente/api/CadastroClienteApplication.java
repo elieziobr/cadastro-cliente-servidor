c0834ce7d6392d54c1d7cc2195070878838803b7
eliezio.soares <eliezio.soares@sicoob.com.br>
2020-06-27 23:41:51 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/soares/cadastrocliente/api/CadastroClienteApplication.java
@@ -0,0 +1,13 @@
+package com.soares.cadastrocliente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroClienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroClienteApplication.class, args);
+    }
+
+}
